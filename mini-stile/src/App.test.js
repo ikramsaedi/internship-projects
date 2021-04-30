eba56419508611a60d58c0a5fbb3ff060e2f89bf
ikramsaedi <ikramsaedi@gmail.com>
2021-04-30 11:50:56 +1000

    write tests for assigning staff and students to classes

--- mini-stile/src/App.test.js
+++ mini-stile/src/App.test.js
@@ -1,8 +1,19 @@
 import { School } from "./School";
 
+let stileSchool;
+let primaryTeacher;
+let assistantTeacher;
+let DOB;
+let student;
+let subject;
+
+beforeEach(() => {
+  //create successful variable
+  stileSchool = new School("Stile School", "Melbourne", "Seedling");
+});
+
 describe("Start of Term", () => {
   test("Creating a school", () => {
-    const stileSchool = new School("Stile School", "Melbourne", "Seedling");
     expect(stileSchool).toEqual({
       _name: "Stile School",
       _suburb: "Melbourne",
@@ -14,7 +25,6 @@ describe("Start of Term", () => {
   });
 
   test("Hiring a teacher", () => {
-    const stileSchool = new School("Stile School", "Melbourne", "Seedling");
     let DOB = new Date(1974, 6, 8);
     let testID = stileSchool.hireTeacher(
       "Ms Jenkins",
@@ -34,7 +44,6 @@ describe("Start of Term", () => {
   });
 
   test("Creating a subject", () => {
-    const stileSchool = new School("Stile School", "Melbourne", "Seedling");
     let DOB = new Date(1974, 6, 8);
     let staffID = stileSchool.hireTeacher(
       "Ms Jenkins",
@@ -61,7 +70,6 @@ describe("Start of Term", () => {
   });
 
   test("Enrol a student", () => {
-    const stileSchool = new School("Stile School", "Melbourne", "Seedling");
     let DOB = new Date(2004, 6, 8);
     let testID = stileSchool.enrolStudent(
       "Polly Cracker",
@@ -78,6 +86,62 @@ describe("Start of Term", () => {
         _faveSuit: "Hearts",
         _grades: {},
       },
+    });
+  });
+
+  test("Assign assistant teacher", () => {
+    let DOB = new Date(1974, 6, 8);
+    let primaryID = stileSchool.hireTeacher(
+      //primary teacher
+      "Ms Jenkins",
+      DOB,
+      "Kill the principal and take their job"
+    );
+
+    let assistantID = stileSchool.hireTeacher(
+      //assistant teacher
+      "Mr Smith",
+      DOB,
+      "Doing the bare minimum until they retire"
+    );
+
+    let subjectID = stileSchool.createSubject(
+      "Year 10 Maths",
+      "10B",
+      ["Monday 10:00 am", "Thursday 12:30 pm"],
+      stileSchool.teachers[primaryID]
+    );
+    stileSchool.assignAssistantTeacher(subjectID, assistantID);
+
+    expect(stileSchool._subjects[subjectID][_assistantTeacher]).toEqual(
+      stileSchool._teachers[assistantID]
+    ); //check specific property
+  });
+
+  test("Assign student to a class", () => {
+    let teacherDOB = new Date(1974, 6, 8);
+    let teacherID = stileSchool.hireTeacher(
+      //primary teacher
+      "Ms Jenkins",
+      teacherDOB,
+      "Kill the principal and take their job"
+    );
+    let subjectID = stileSchool.createSubject(
+      "Year 10 Maths",
+      "10B",
+      ["Monday 10:00 am", "Thursday 12:30 pm"],
+      stileSchool.teachers[teacherID]
+    );
+    let studentDOB = new Date(2004, 6, 8); //student
+    let studentID = stileSchool.enrolStudent(
+      "Polly Cracker",
+      studentDOB,
+      "Cheese and biscuits",
+      "Hearts"
+    );
+    stileSchool.assignStudent(subjectID, studentID);
+    expect(stileSchool._subjects[subjectID][_students]).toEqual({
+      [studentID]: [stileSchool._students[studentID]], //this is students obj with studentID in it, referring to the same obj as the one in the school
     });
   });
 });
