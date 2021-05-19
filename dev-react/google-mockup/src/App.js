9ef14d8eda361d6b1a9e3dfc302bc475d8560b3c
aztlanleuc <dm.phone766@gmail.com>
2021-05-19 15:25:28 +1000

    initial work. timeboxed, may come back at later date

--- dev-react/google-mockup/src/App.js
+++ dev-react/google-mockup/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./google-logo.png";
+
+function Button(props) {
+  return <button className={props.className}>{props.value}</button>;
+}
+
+function Content() {
+  return (
+    <div>
+      <img src={                                                     } alt="alt text" width="300px" />
+      <Button value="Google Search" className="content-button" />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Content />
     </div>
   );
 }
