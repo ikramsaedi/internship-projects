fcaa4197a673778cccab13caee4ab973c51f5703
aztlanleuc <dm.phone766@gmail.com>
2021-05-20 16:15:48 +1000

    lose lives when guessing incorrectly

--- dev-react/hangman/src/App.js
+++ dev-react/hangman/src/App.js
@@ -71,35 +71,25 @@ class LetterSelectors extends React.Component {
 }
 
 class WordDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.wordArray = this.createWordArray(this.props.gameWord);
-    this.blankArray = this.createBlankArray(this.props.gameWord);
-  }
-
-  createWordArray(word) {
-    let output = [];
-    for (let letter of word) {
-      output.push(letter);
-    }
-    return output;
-  }
-
-  createBlankArray(word) {
-    let output = [];
-    for (let letter in word) {
-      output.push("_ ");
-    }
-    return output;
-  }
-
   render() {
+    return (
+      <div className={this.props.className}>{this.props.displayArray}</div>
+    );
+  }
+  }
+
+class Score extends React.Component {
+  render() {
+    return (
+      <div className={this.props.className}>
     for (let element in this.wordArray) {
       if (this.props.lettersGuessed.includes(this.wordArray[element])) {
         this.blankArray[element] = this.wordArray[element];
       }
     }
     return <div className={this.props.className}>{this.blankArray}</div>;
+      </div>
+    );
   }
 }
 
@@ -107,11 +97,13 @@ class Game extends React.Component {
   render() {
     return (
       <div className={this.props.className}>
+        <Score className="score" livesRemaining={this.props.livesRemaining} />
         <WordDisplay
           className="word-display"
           gameWord={this.props.gameWord}
+          displayArray={this.props.displayArray}
           lettersGuessed={this.props.lettersGuessed}
-          renderLetter={(i) => this.props.letterRender(i)}
+          loseLife={() => this.props.loseLife()}
         />
         <LetterSelectors
           lettersActive={this.props.lettersActive}
@@ -158,27 +150,52 @@ class App extends React.Component {
         z: true,
       },
       lettersGuessed: [],
+      livesRemaining: null,
+      displayArray: null,
     };
   }
 
   startGame(i) {
     const word = this.words[Math.floor(Math.random() * this.words.length)];
-    this.setState({ gameState: "playing", gameWord: word });
+
+    let array = [];
+    for (let letter in word) {
+      array.push("_ ");
+    }
+
+    this.setState({
+      gameState: "playing",
+      gameWord: word,
+      livesRemaining: 15,
+      displayArray: array,
+    });
   }
 
   submitLetter(event) {
     const letterValue = event.target.innerText;
     const lettersGuessed = this.state.lettersGuessed.concat(letterValue);
     const lettersActive = this.state.lettersActive;
+    let displayArray = this.state.displayArray;
+    let livesRemaining = this.state.livesRemaining;
+
     lettersActive[letterValue] = false;
+
+    if (this.state.gameWord.includes(letterValue)) {
+      for (let index in this.state.gameWord) {
+        if (this.state.gameWord[index] === letterValue) {
+          displayArray[index] = letterValue;
+        }
+      }
+    } else {
+      livesRemaining -= 1;
+    }
+
     this.setState({
       lettersActive: lettersActive,
       lettersGuessed: lettersGuessed,
+      displayArray: displayArray,
+      livesRemaining: livesRemaining,
     });
-  }
-
-  renderLetter(value) {
-    return <h1 className="display-letter">{value}</h1>;
   }
 
   render() {
@@ -189,12 +206,13 @@ class App extends React.Component {
             <Game
               className="game"
               gameWord={this.state.gameWord}
+              displayArray={this.state.displayArray}
+              livesRemaining={this.state.livesRemaining}
               lettersActive={this.state.lettersActive}
               lettersGuessed={this.state.lettersGuessed}
               letterSubmit={(i) => this.submitLetter(i)}
-              letterRender={(i) => this.renderLetter(i)}
+              loseLife={() => this.loseLife()}
             />
-            <p>{this.state.lettersGuessed}</p>
           </div>
         );
       case "unstarted":
