dff747a0b238fd7d517c0919fa97b32612275f1b
aztlanleuc <dm.phone766@gmail.com>
2021-05-31 11:49:22 +1000

    pacman now faces the direction he's going to move

--- dev-react/pacman/src/Game.js
+++ dev-react/pacman/src/Game.js
@@ -5,26 +5,72 @@ import "./App.css";
 import pacman from "./resources/pacman.png";
 
 function Pacman(props) {
-  return <img src={props.img} alt="pacman sprite" />;
+  return (
+    <img
+      src={props.img}
+      alt="pacman sprite"
+      style={{ transform: "rotate(" + props.dir + ")" }}
+    />
+  );
 }
 
 class UnstyledGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.validKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
+    this.state = { currentDirection: 0 };
+  }
+
+  handleKey(event) {
+    console.log(event.key);
+    if (this.validKeys.includes(event.key)) {
+      console.log("valid");
+      let dir;
+      switch (event.key) {
+        case "ArrowUp":
+          dir = "270deg";
+          break;
+        case "ArrowDown":
+          dir = "90deg";
+          break;
+        case "ArrowLeft":
+          dir = "180deg";
+          break;
+        case "ArrowRight":
+          dir = "0deg";
+          break;
+      }
+
+      this.setState({ currentDirection: dir });
+    }
+  }
+
+  componentDidMount() {
+    this.boundEventListener = (event) => this.handleKey(event);
+    document.addEventListener("keydown", this.boundEventListener);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.boundEventListener);
+  }
 
   render() {
     return (
       <div id="game" className={this.props.className}>
-        <Pacman img={pacman} />
+        <Pacman img={pacman} dir={this.state.currentDirection} />
       </div>
     );
   }
 }
 
 const Game = styled(UnstyledGame)`
+  box-sizing: border-box;
   background-color: black;
   color: white;
   width: 60%;
   height: 800px;
   margin: 0 auto;
+  padding: 10px;
 `;
 
 export default Game;
