15947a7cb77c93eae4a66beb9f470fe5d8730f5d
aztlanleuc <dm.phone766@gmail.com>
2021-06-01 09:48:27 +1000

    pacman can now start and stop 'moving' currently he does not actually move, but the move method is successfully being repeated, and currently just console logging 'moving'

--- dev-react/pacman/src/Game.js
+++ dev-react/pacman/src/Game.js
@@ -4,45 +4,87 @@ import "./App.css";
 
 import pacman from "./resources/pacman.png";
 
-function Pacman(props) {
-  return (
-    <img
-      src={props.img}
-      alt="pacman sprite"
-      style={{ transform: "rotate(" + props.dir + ")" }}
-    />
-  );
+class Pacman extends React.Component {
+  componentDidUpdate() {
+    if (this.props.isMoving) {
+      this.timer = setInterval(() => this.move(), 1000);
+    }
+
+    if (!this.props.isMoving) {
+      clearInterval(this.timer);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  move() {
+    console.log("moving!");
+
+    // inside this, will need to run my collision checker and update pacmans position
+  }
+
+  render() {
+    if (this.props.isMoving) {
+      console.log("it's alIVE");
+    }
+
+    return (
+      <img
+        src={this.props.img}
+        alt="pacman sprite"
+        height={this.props.size}
+        width={this.props.size}
+        style={{ transform: "rotate(" + this.props.dir + ")" }}
+      />
+    );
+  }
 }
 
 class UnstyledGame extends React.Component {
   constructor(props) {
     super(props);
     this.validKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-    this.state = { currentDirection: 0 };
+    this.gridSize = 32;
+    this.state = { currentDirection: 0, isPacmanMoving: false };
   }
 
   handleKey(event) {
-    console.log(event.key);
-    if (this.validKeys.includes(event.key)) {
-      console.log("valid");
-      let dir;
-      switch (event.key) {
-        case "ArrowUp":
-          dir = "270deg";
-          break;
-        case "ArrowDown":
-          dir = "90deg";
-          break;
-        case "ArrowLeft":
-          dir = "180deg";
-          break;
-        case "ArrowRight":
-          dir = "0deg";
-          break;
-      }
-
-      this.setState({ currentDirection: dir });
+    if (event.repeat) {
+      return;
     }
+
+    console.log(event.key);
+
+    if (event.key === " ") {
+      console.log("space bar");
+      this.setState({ isPacmanMoving: !this.state.isPacmanMoving });
+    }
+
+    // if (this.validKeys.includes(event.key)) {
+    //   console.log("valid");
+    //   let dir;
+    //   switch (event.key) {
+    //     case "ArrowUp":
+    //       dir = "270deg";
+    //       break;
+    //     case "ArrowDown":
+    //       dir = "90deg";
+    //       break;
+    //     case "ArrowLeft":
+    //       dir = "180deg";
+    //       break;
+    //     case "ArrowRight":
+    //       dir = "0deg";
+    //       break;
+    //     default: // pass
+    //   }
+
+    //   this.setState({ currentDirection: dir });
+    // }
   }
 
   componentDidMount() {
@@ -57,7 +99,12 @@ class UnstyledGame extends React.Component {
   render() {
     return (
       <div id="game" className={this.props.className}>
-        <Pacman img={pacman} dir={this.state.currentDirection} />
+        <Pacman
+          img={pacman}
+          dir={this.state.currentDirection}
+          size={this.gridSize}
+          isMoving={this.state.isPacmanMoving}
+        />
       </div>
     );
   }
