6d78ae61784bffd180d4e5d2a9f7bacdd236251a
ikramsaedi <ikramsaedi@gmail.com>
2021-07-28 14:51:56 +1000

    can now parse commands from the command line using the optparse library

--- ikram-rspec/shauno-buttons/cli.rb
+++ ikram-rspec/shauno-buttons/cli.rb
@@ -1,1 +1,16 @@
 require 'optparse'
+
+class Parser
+    def self.parse(args)
+        options = {}
+        opts = OptionParser.new do |opts|
+            opts.on("-h", "--hello", "This says hello to the user") {
+                puts "hello world!"
+            }
+        end
+        opts.parse(args)
+        options
+    end
+end
+
+options = Parser.parse(ARGV)
