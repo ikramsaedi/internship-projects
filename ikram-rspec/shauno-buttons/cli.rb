b63fd01b8b9222f5565f715746fc7a148465f33a
ikramsaedi <ikramsaedi@gmail.com>
2021-07-29 14:19:40 +1000

    CLI command now exists to list all buttons along with their associated reason and developer

--- ikram-rspec/shauno-buttons/cli.rb
+++ ikram-rspec/shauno-buttons/cli.rb
@@ -1,25 +1,29 @@
 require 'optparse'
 require 'mysql2'
 
-client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => "shauno_buttons")
-#puts client
-results = client.query("SELECT * FROM reason_pairings GROUP BY reason_id")
-
-results.each do |row|
-    p row
-end
-
-headers = results.fields
-puts types
-
 class Parser
+    @@client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => "shauno_buttons")
+
     def self.parse(args)
         options = {}
         opts = OptionParser.new do |opts|
-            opts.on("-h", "--hello [PLANET]", "This says hello to the user") { |planet|
+            opts.on("-h", "--hello [PLANET]", "This says hello to the PLANET provided (defaults to world)") { |planet|
                 options[:planet] = planet
                 puts "hello #{planet ? planet : "world"}!"
             }
+            opts.on("-b", "--list-buttons", "This will return all the buttons, along with assigned reason and developer") {
+                results = @@client.query(
+                    "SELECT developer_pairings.button_id, reason, name FROM reason_pairings 
+                    JOIN developer_pairings ON reason_pairings.button_id=developer_pairings.button_id 
+                    JOIN reasons ON reason_pairings.reason_id=reasons.id 
+                    JOIN developers ON developer_pairings.developer_id=developers.id"
+                    )
+
+                results.each do |row|
+                    p row
+                end
+            }
+            
         end
         opts.parse(args)
         options
