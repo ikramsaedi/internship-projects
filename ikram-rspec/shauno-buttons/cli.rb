b27dd53c9cdeeae6e23618978af5aa5d93ed2bca
ikramsaedi <ikramsaedi@gmail.com>
2021-07-29 11:41:46 +1000

    connected to db and can query the db

--- ikram-rspec/shauno-buttons/cli.rb
+++ ikram-rspec/shauno-buttons/cli.rb
@@ -1,11 +1,24 @@
 require 'optparse'
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => "shauno_buttons")
+#puts client
+results = client.query("SELECT * FROM reason_pairings GROUP BY reason_id")
+
+results.each do |row|
+    p row
+end
+
+headers = results.fields
+puts types
 
 class Parser
     def self.parse(args)
         options = {}
         opts = OptionParser.new do |opts|
-            opts.on("-h", "--hello", "This says hello to the user") {
-                puts "hello world!"
+            opts.on("-h", "--hello [PLANET]", "This says hello to the user") { |planet|
+                options[:planet] = planet
+                puts "hello #{planet ? planet : "world"}!"
             }
         end
         opts.parse(args)
@@ -14,3 +27,4 @@ class Parser
 end
 
 options = Parser.parse(ARGV)
+
