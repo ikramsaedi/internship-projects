2c6bdb28c729d089461f941c4b404bd18a25da7c
ikramsaedi <ikramsaedi@gmail.com>
2021-08-02 10:31:47 +1000

    Subcommands now run actual queries rather than just creating a hash

--- ikram-rspec/shauno-buttons/cli.rb
+++ ikram-rspec/shauno-buttons/cli.rb
@@ -1,6 +1,7 @@
 require 'optparse'
 require 'optimist'
 require 'mysql2'
+require 'pp'
 
 client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => "shauno_buttons")
 
@@ -30,7 +31,7 @@ cmd_opts = case cmd
         Optimist::die "Unknown subcommand #{cmd.inspect}" if cmd # if no subcommand is given, we don't want it to die, just move on
     end
 
-cmd_opts[cmd.to_sym] = true if cmd # similarly, if there is no subcommand given, there is no command to store
+cmd_opts[:cmd] = cmd if cmd # similarly, if there is no subcommand given, there is no command to store
 
 p main_opts
 p cmd_opts
@@ -38,3 +39,29 @@ p cmd_opts
 if main_opts[:hello_given]
     puts "Hello #{main_opts[:hello]}!"
 end
+
+case cmd_opts[:cmd] 
+    when "list_buttons"
+        results = client.query(
+            "SELECT developer_pairings.button_id, reason, name FROM reason_pairings 
+            JOIN developer_pairings ON reason_pairings.button_id=developer_pairings.button_id 
+            JOIN reasons ON reason_pairings.reason_id=reasons.id 
+            JOIN developers ON developer_pairings.developer_id=developers.id"
+        )
+
+        results.each do |row|
+            p row
+        end
+    when "add_event"
+        begin
+            results = client.query(
+                "INSERT INTO events (button_id, timestamp, developers_id, reason_id) VALUES (#{cmd_opts[:button_id]}, '#{cmd_opts[:timestamp]}', #{cmd_opts[:developer]}, #{cmd_opts[:reason]});"
+            )
+            p "Insert successful!"
+        rescue => err
+            p "Insert failed."
+            p err
+        end
+    else # nothing?
+end
+
