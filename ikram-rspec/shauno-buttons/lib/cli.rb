f1ecbd0c4b07deccac21d78d76bf2b2dfdf7cad8
ikramsaedi <ikramsaedi@gmail.com>
2021-08-03 16:03:04 +1000

    list buttons test is successful

--- ikram-rspec/shauno-buttons/lib/cli.rb
+++ ikram-rspec/shauno-buttons/lib/cli.rb
@@ -4,23 +4,19 @@ require 'pp'
 
 database_name = "shauno_buttons"
 if ENV["DB_NAME"]
-    puts "we r in the env if statement"
+    # puts "we r in the env if statement"
     database_name = ENV["DB_NAME"]
-    puts database_name
+    # puts database_name
 end
 $client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => database_name, :flags => Mysql2::Client::MULTI_STATEMENTS)
 
 def list_buttons()
-    results = $client.query(
+    puts "querying database..."
+    return $client.query(
         "SELECT developer_pairings.button_id, reason, name FROM reason_pairings 
         JOIN developer_pairings ON reason_pairings.button_id=developer_pairings.button_id 
         JOIN reasons ON reason_pairings.reason_id=reasons.id 
-        JOIN developers ON developer_pairings.developer_id=developers.id"
-    )
-
-    results.each do |row|
-        p row
-    end
+        JOIN developers ON developer_pairings.developer_id=developers.id;")
 end
 
 def add_event(button_id, timestamp, developer, reason) 
@@ -46,12 +42,10 @@ cmd = ARGV.shift
 
 cmd_opts = case cmd
     when "list_buttons"
-        puts "hey"
         Optimist::options do
             banner "This subcommand does not have any options"
         end
     when "add_event"
-        puts "we in add event"
         Optimist::options do
             opt :button_id, "The ID of the button in the event", :type => :int, :required => true
             opt :timestamp, "The timestamp of the event", :type => :string, :required => true
@@ -61,14 +55,14 @@ cmd_opts = case cmd
         end
 
     else 
-        puts "we dieeeee"
+        # puts "we dieeeee"
         Optimist::die "Unknown subcommand #{cmd.inspect}" if cmd # if no subcommand is given, we don't want it to die, just move on
     end
 
 cmd_opts[:cmd] = cmd if cmd # similarly, if there is no subcommand given, there is no command to store
 
-p main_opts
-p cmd_opts
+# p main_opts
+# p cmd_opts
 
 if main_opts[:hello_given]
     puts "Hello #{main_opts[:hello]}!"
@@ -77,7 +71,13 @@ end
 if cmd_opts 
     case cmd_opts[:cmd] 
         when "list_buttons"
-            list_buttons
+            puts "list buttons subcommand called"
+
+            results = list_buttons
+
+            results.each do |row|
+                p row
+            end
         when "add_event"
             add_event(cmd_opts[:button_id], cmd_opts[:timestamp], cmd_opts[:developer], cmd_opts[:reason])
         else # nothing?
--- ikram-rspec/shauno-buttons/spec/cli_spec.rb
+++ ikram-rspec/shauno-buttons/spec/cli_spec.rb
@@ -4,21 +4,16 @@ require 'pp'
 
 require 'cli'
 
-p $client
-
 describe 'running query functions' do
-    #before hook
     before(:all) do
 
         result = $client.query(File.read("./data/buttons.sql"))
 
         while $client.next_result #this checks if theres another result left to handle
-            # puts result
             result = $client.store_result
         end
     end
 
-    #after hook
     after(:all) do
 
         result = $client.query(File.read("./data/truncate.sql"))
@@ -30,7 +25,22 @@ describe 'running query functions' do
     
     context 'list_buttons' do
         it "lists the correct buttons" do
+            # so how do we test it?
+            # counting will be easiest I guess
+            expected_num = 6
+
+            result = list_buttons
+            puts "this is class #{result.class}"
             
+
+            puts "i can use length: #{result.respond_to?(:length)}"
+
+            puts "the size is #{result.size}"
+
+ 
+            expect(result.size).to eq(expected_num)
         end
+
+        
     end
 end
