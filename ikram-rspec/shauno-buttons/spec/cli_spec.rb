6cdd4ca01416b7f0fc8945775036d8d48c57af50
ikramsaedi <ikramsaedi@gmail.com>
2021-08-04 10:22:51 +1000

    list buttons tests complete

--- ikram-rspec/shauno-buttons/spec/cli_spec.rb
+++ ikram-rspec/shauno-buttons/spec/cli_spec.rb
@@ -24,22 +24,31 @@ describe 'running query functions' do
     end
     
     context 'list_buttons' do
-        it "lists the correct buttons" do
+        it "returns the correct number of buttons" do
             # so how do we test it?
             # counting will be easiest I guess
             expected_num = 6
 
             result = list_buttons
-            puts "this is class #{result.class}"
-            
 
-            puts "i can use length: #{result.respond_to?(:length)}"
-
-            puts "the size is #{result.size}"
-
- 
             expect(result.size).to eq(expected_num)
         end
+
+        it "lists the correct reason with the buttons" do
+            result = list_buttons
+
+            correct_reason = "CI is broken"
+
+            expect(result.first["reason"]).to eq(correct_reason)
+        end
+
+        it "lists the correct developer with the buttons" do
+            result = list_buttons
+            correct_name = "Ikram Saedi"
+
+            expect(result.first["name"]).to eq(correct_name)
+        end
+    end
 
         
     end
