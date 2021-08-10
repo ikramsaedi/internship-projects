8143d13d4ae03d94aac007c2e7bef20d2005fb67
ikramsaedi <ikramsaedi@gmail.com>
2021-08-10 13:32:45 +1000

    subcommands are now associated with the add_button, invalidate_button and reassign_button method

--- shauno-buttons/lib/cli.rb
+++ shauno-buttons/lib/cli.rb
@@ -13,19 +13,23 @@ end
 $client = Mysql2::Client.new(:host => "intern-party-2.cpj2kqopsdsq.ap-southeast-2.rds.amazonaws.com", :username => "admin", :password => ENV["DB_PASSWORD"], :database => database_name, :flags => Mysql2::Client::MULTI_STATEMENTS)
 
 
-SUB_COMMANDS = %w(list_buttons, add_event, invalidate_event) #%w makes these things into a list
+SUB_COMMANDS = %w(list_buttons, add_event, invalidate_event, add_button, invalidate_button, reassign_button) #%w makes these things into a list
 
 commands_info = "
 Subcommands include:
     list_buttons        return a list of all buttons, along with their associated reason and developer
     add_event           adds a new event to the events table
-    invalidate_event    sets an event to be ignored by statistics"
+    invalidate_event    sets an event to be ignored by statistics
+    add_button          adds a button to the table and associates it with the given reason and ID
+    invalidate_button   marks the given button as inactive and removes its associations
+    reassign_button     removes the given buttons old association and adds a new one. one or both out of the reason and developer must be given"
 
 main_opts = Optimist::options do
     opt :hello, "Says hello to the given thing", :default => "world"
     opt :help, "Show this message"
     stop_on SUB_COMMANDS
     banner commands_info
+    banner "Use 'ruby <file path> SUBCOMMAND --help' for more information"
 end
 
 cmd = ARGV.shift
@@ -45,17 +49,31 @@ cmd_opts = case cmd
     when "invalidate_event"
         Optimist::options do
             opt :developer_id, "The developer who is performing the action", :type => :int, :required => :true
-            opt :button_id, "The button used in the event", :type => :int, :required => :true
+            opt :button_id, "The button ID used in the event", :type => :int, :required => :true
             opt :timestamp, "The timestamp of the event", :type => :string, :required => :true
+        end
+    when "add_button"
+        Optimist::options do
+            opt :uuid, "The uuid of the physical button", :type => :string, :required => :true
+            opt :reason_id, "The reason ID the button should be assigned to", :type => :int, :required => :true
+            opt :developer_id, "The developer ID the button should be assigned to", :type => :int, :required => :true
+        end
+    when "invalidate_button"
+        Optimist::options do 
+            opt :developer_id, "The ID of the developer who wants to invalidate the event", :type => :int, :required => :true
+            opt :button_id, "The ID of the button to be invalidated", :type => :int, :required => :true
+        end
+    when "reassign_button"
+        Optimist::options do
+            opt :button_id, "The ID of the button to be reassigned", :type => :int, :required => :true
+            opt :reason_id, "The new reason ID the button should be assigned to", :type => :int
+            opt :developer_id, "The new developer ID the button should be assigned to", :type => :int
         end
     else 
         Optimist::die "Unknown subcommand #{cmd.inspect}" if cmd # if no subcommand is given, we don't want it to die, just move on
     end
 
 cmd_opts[:cmd] = cmd if cmd # similarly, if there is no subcommand given, there is no command to store
-
-# p main_opts
-# p cmd_opts
 
 if main_opts[:hello_given]
     puts "Hello #{main_opts[:hello]}!"
