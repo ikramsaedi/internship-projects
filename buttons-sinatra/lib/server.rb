e7bb9635ac0ce93b5753a2791e76eb0b0dca9c8b
ikramsaedi <ikramsaedi@gmail.com>
2021-08-19 14:47:37 +1000

    all buttons related tests pass

--- buttons-sinatra/lib/server.rb
+++ buttons-sinatra/lib/server.rb
@@ -37,24 +37,45 @@ get '/buttons/:id' do #404 status code if button doesnt exist
     if !result.nil?
         JSON.generate(result)
     else result
-        [403, ["Error 403: You can't see that ;-;\n"]]
+        [404, ["Error 404! The thing you are looking for does not exist\n"]]
     end
 end
 
-post '/add_event' do
+get '/add_event/:button_id/:timestamp/:developers_id/:reason_id' do
+    begin 
+        Integer(params[:button_id])
+    rescue
+        return [400, ["Error 400: Button ID is not an integer!\n"]]
+    end
+
+    begin 
+        Integer(params[:developers_id])
+    rescue ArgumentError
+        return [400, ["Error 400: Developer ID is not an integer!\n"]]
+    end
+
+    begin
+        Integer(params[:reason_id])
+    rescue
+        return [400, ["Error 400: Reason ID is not an integer!\n"]]
+    end
+
+    begin
+        DateTime.strptime(params[:timestamp], "%Y-%m-%d %H:%M:%S")
+    rescue => e
+        return [400, ["Error 400: Timestamp is not valid!\n"]]
+    end
+
     statement = $client.prepare("INSERT INTO events (button_id, timestamp, developers_id, reason_id) VALUES (?, ?, ?, ?);")
-    statement.execute(button_id, timestamp, developer, reason)
+    statement.execute(params[:button_id], params[:timestamp], params[:developers_id], params[:reason_id])
 end
 
 get '/is_admin/:developer_id' do
-    puts "call is_admin"
     statement = $client.prepare("SELECT is_admin FROM developers WHERE id=?")
     result = statement.execute(params["developer_id"]).first
 
     if result && result["is_admin"] == 1 #check if result exists and if the dev is an admin
-        pp session
         session[:is_admin] = "true"
-        pp session
         redirect params["previous"]
     else
         [403, ["Please check your permissions and try again."]]
@@ -62,8 +83,6 @@ get '/is_admin/:developer_id' do
 end
 
 get '/invalidate_event/:developer_id/:button_id/:timestamp' do
-    pp session
-
     if session[:is_admin] == "true"
         session[:is_admin] = nil
         statement = $client.prepare("UPDATE events SET to_ignore = 1 WHERE button_id=? AND timestamp=?;")
@@ -74,4 +93,80 @@ get '/invalidate_event/:developer_id/:button_id/:timestamp' do
     end
 end
 
-#### http://localhost:4567/invalidate_event/3/2/23131
+get '/add_button/:uuid/:reason_id/:developer_id' do
+    begin
+        statement = $client.prepare("INSERT INTO buttons (uuid) VALUES (?);")
+        statement.execute(params[:uuid])
+    rescue => e
+        return [409, ["This button already exists in the database!"]]
+    end
+
+    statement = $client.prepare("SELECT button_id FROM buttons WHERE uuid=?;")
+    result = statement.execute(params[:uuid])
+    id = result.first["button_id"]
+
+    statement = $client.prepare("INSERT INTO reason_pairings (button_id, reason_id) VALUES (?, ?);")
+    statement.execute(id, params[:reason_id])
+
+    statement = $client.prepare("INSERT INTO developer_pairings (button_id, developer_id) VALUES (?, ?);")
+    statement.execute(id, params[:developer_id])
+end
+
+get '/invalidate_button/:developer_id/:button_id' do
+    
+    if session["is_admin"] == "true"
+        if params[:button_id]
+            statement = $client.prepare("UPDATE buttons SET is_active=0 WHERE button_id=?;")
+            statement.execute(params[:button_id])
+
+            statement = $client.prepare("UPDATE reason_pairings SET CURRENT=0 WHERE button_id=?;")
+            statement.execute(params[:button_id])
+
+            statement = $client.prepare("UPDATE developer_pairings SET CURRENT=0 WHERE button_id=?;")
+            statement.execute(params[:button_id])
+        else
+            raise InvalidDataError
+        end
+    else
+        redirect to("/is_admin/#{params["developer_id"]}?previous=#{URI.encode(request.url)}") #test not following the redirect
+    end
+end
+
+get "/reassign_button/:button_id" do
+    # check if at least one of the things to update has been given, and throw an error if not
+    if !params[:reason_id] && !params[:developer_id]
+        return [400, "Error 400: You are missing data in your request"]
+    end
+
+    #check that the button provided has not been deactivated
+    statement = $client.prepare("SELECT is_active FROM buttons WHERE button_id=?;")
+    result = statement.execute(params[:button_id]).first
+
+    if result["is_active"] == 0
+        return [404, ["The button you asked for cannot be found"]]
+    end
+
+    if params[:reason_id] # if a new reason is given
+
+        # invalidate any existing reasons
+        statement = $client.prepare("UPDATE reason_pairings SET CURRENT=0 WHERE button_id=?;")
+        statement.execute(params[:button_id])
+
+        # add the new reason
+        statement = $client.prepare("INSERT INTO reason_pairings (button_id, reason_id) VALUES (?, ?);")
+        statement.execute(params[:button_id], params[:reason_id])
+    end
+
+    if params[:developer_id] # do the same if a new developer was given
+
+        # invalidate any existing developers
+        statement = $client.prepare("UPDATE developer_pairings SET CURRENT=0 WHERE button_id=?;")
+        statement.execute(params[:button_id])
+
+        # add the new developer
+        statement = $client.prepare("INSERT INTO developer_pairings (button_id, developer_id) VALUES (?, ?);")
+        statement.execute(params[:button_id], params[:developer_id])
+    end
+end
+
+#### http://localhost:4567/add_event/
--- buttons-sinatra/spec/server_spec.rb
+++ buttons-sinatra/spec/server_spec.rb
@@ -4,6 +4,8 @@ require 'irb'
 
 require 'server'
 
+require 'uri'
+
 describe "Sinatra App" do
     include Rack::Test::Methods
 
@@ -28,10 +30,298 @@ describe "Sinatra App" do
     context "/buttons" do
         it "returns buttons" do
             get "/buttons"
-            data = JSON.parse last_response.body
-            pp data # pp = puts but calls .inspect
-            # binding.irb
-            expect(data.size).to eq(6)
+            result = JSON.parse last_response.body
+            expect(result.size).to eq(6)
+        end
+
+        it "lists the correct reason with the buttons" do
+            get "/buttons"
+            result = JSON.parse last_response.body
+            correct_reason = "CI is broken"
+            expect(result.first["reason"]).to eq(correct_reason)
+        end
+
+        it "lists the correct developer with the buttons" do
+            get "/buttons"
+            result = JSON.parse last_response.body
+            correct_name = "Ikram Saedi"
+
+            expect(result.first["name"]).to eq(correct_name)
+        end
+    end
+
+    context "/buttons/:id" do
+        it "returns the correct button uuid and is_active value" do
+            get "/buttons/1"
+            result = JSON.parse last_response.body
+
+            correct_uuid = "d167cd3a-2314-47d7-8d0a-e55bc69608df"
+            expect(result["uuid"]).to eq(correct_uuid)
+            expect(result["is_active"]).to eq(1)
+        end
+
+        it "throws a 404 error when button_id passed in does not exist" do
+            get "buttons/500"
+
+            expect(last_response.status).to eq(404)
+        end
+    end
+
+    context "add_event" do
+        let(:button_id) { 2 }
+        let(:timestamp) { "2021-3-2 06:24:49" }
+        let(:developer_id) { 1 }
+        let(:reason_id) { 3 }
+
+        it "successfully adds event when given valid information" do
+            # Subcommands::add_event(button_id, timestamp, developer_id, reason_id)
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+            # pp last_response.body
+            expect(last_response.status).to eq(200)
+
+            result = $client.query("SELECT button_id, DATE_FORMAT(timestamp, '%Y-%c-%e %H:%i:%s') AS timestamp, developers_id, reason_id, to_ignore FROM events WHERE button_id=#{button_id} AND timestamp='#{timestamp}';")
+
+            result = result.first
+
+            expected = {"button_id" => button_id, "timestamp" => timestamp, "developers_id" => developer_id, "reason_id" => reason_id, "to_ignore" => 0}
+
+            expect(result).to eq(expected)
+        end
+
+        it "does not insert event into table when timestamp is missing" do
+            timestamp = "" # this causes an error in URI.encode. how can we get around that without making it a weird test case (like skipping the encode)
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert event into table when button is missing" do
+            button_id = nil
+
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert event into table when developer_id is missing" do
+            developer_id = nil
+
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert event into table when reason_id is missing" do
+            reason_id = nil
+
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert event into table when timestamp is invalid" do
+            timestamp = "gobblydegook"
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(400)
+        end
+
+        it "does not insert event into table when button is invalid" do
+            button_id = "gobblydegook"
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(400)
+        end
+
+        it "does not insert event into table when developer_id is invalid" do
+            developer_id = "gobblydegook"
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(400) 
+        end
+
+        it "does not insert event into table when reason_id is invalid" do
+            reason_id = "gobblydegook"
+            get "/add_event/#{button_id}/#{URI.encode(timestamp)}/#{developer_id}/#{reason_id}"
+
+            expect(last_response.status).to eq(400)
+        end
+    end
+
+    context "add button" do
+        let(:uuid) { "fa22866c-f8af-11eb-9a03-0242ac130003" }
+        let(:reason_id) { 3 }
+        let(:developer_id) { 1 }
+
+        it "adds button successfully" do
+            get "/add_button/#{uuid}/#{reason_id}/#{developer_id}"
+            expect(last_response.status).to eq(200)
+
+            statement = $client.prepare("SELECT button_id, uuid FROM buttons WHERE uuid=?")
+            result = statement.execute(uuid)
+            result = result.first
+
+            expect(result["uuid"]).to eq(uuid)
+
+            db_button_id = result["button_id"]
+
+            statement = $client.prepare("SELECT button_id, reason_id FROM reason_pairings WHERE button_id=?")
+            result = statement.execute(db_button_id)
+            result = result.first
+
+            expect(result["button_id"]).to eq(db_button_id)
+            expect(result["reason_id"]).to eq(reason_id)
+
+            statement = $client.prepare("SELECT button_id, developer_id FROM developer_pairings WHERE button_id=?")
+            result = statement.execute(db_button_id)
+            result = result.first
+
+            expect(result["button_id"]).to eq(db_button_id)
+            expect(result["developer_id"]).to eq(developer_id)
+            
+        end
+
+        it "throws an error if the uuid is already in the table" do #this requires adding a constraint
+            uuid = "467fa190-d806-4d45-9eda-08e322d6fccf"
+            get "/add_button/#{uuid}/#{reason_id}/#{developer_id}"
+            expect(last_response.status).to eq(409)
+        end
+
+        it "does not insert into table when uuid is missing" do
+            uuid = nil
+            get "/add_button/#{uuid}/#{reason_id}/#{developer_id}"
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert into table when reason_id is missing" do
+            reason_id = nil
+            get "/add_button/#{uuid}/#{reason_id}/#{developer_id}"
+            expect(last_response.status).to eq(404)
+        end
+
+        it "does not insert into table when developer_id is missing" do
+            developer_id = nil
+            get "/add_button/#{uuid}/#{reason_id}/#{developer_id}"
+            expect(last_response.status).to eq(404)
+        end
+    end
+
+    context "invalidate button" do
+        let(:button_id) { 1 }
+        let(:developer_id) { 3 }
+
+        it "invalidates button successfully" do
+            get "/invalidate_button/#{developer_id}/#{button_id}"
+            follow_redirect!
+            follow_redirect!
+            expect(last_response.status).to eq(200)
+
+            statement = $client.prepare("SELECT is_active FROM buttons WHERE button_id=?;")
+            result = statement.execute(button_id)
+            result = result.first
+
+            expect(result["is_active"]).to eq(0) 
+
+            statement = $client.prepare("SELECT CURRENT FROM reason_pairings WHERE button_id=?;")
+            result = statement.execute(button_id)
+            result = result.first
+
+            expect(result["CURRENT"]).to eq(0)
+
+            statement = $client.prepare("SELECT CURRENT FROM developer_pairings WHERE button_id=?;")
+            result = statement.execute(button_id)
+            result = result.first
+
+            expect(result["CURRENT"]).to eq(0)
+        end
+
+        it "errors when the developer is not an admin" do
+            developer_id = 2
+            get "/invalidate_button/#{developer_id}/#{button_id}"
+            follow_redirect!
+
+            expect(last_response.status).to eq(403)
+        end
+    
+        it "errors when button id is missing" do
+            button_id = nil
+            get "/invalidate_button/#{developer_id}/#{button_id}"
+
+            expect(last_response.status).to eq(404)
+        end
+
+        it "errors when developer id is missing" do
+            developer_id = nil
+            get "/invalidate_button/#{developer_id}/#{button_id}"
+            expect(last_response.status).to eq(404)
+        end
+    end
+    
+    context "reassign button" do
+        let(:button_id) { 2 }
+        let(:new_reason_id) { 2 }
+        let(:new_developer_id) {2}
+
+        it "successfully assigns a new reason" do
+            new_developer_id = nil
+
+            get "/reassign_button/#{button_id}?reason_id=#{new_reason_id}"
+
+            expect(last_response.status).to eq(200)
+
+            statement = $client.prepare("SELECT button_id, reason_id FROM reason_pairings WHERE button_id=? AND reason_id=?;")
+            result = statement.execute(button_id, new_reason_id).first
+
+            expect(result["button_id"]).to eq(button_id)
+            expect(result["reason_id"]).to eq(new_reason_id)
+        end
+        
+        it "successfully assigns a new developer" do
+            new_reason_id = nil
+
+            get "/reassign_button/#{button_id}?developer_id=#{new_developer_id}"
+
+            expect(last_response.status).to eq(200)
+
+            statement = $client.prepare("SELECT button_id, developer_id FROM developer_pairings WHERE button_id=? AND developer_id=?;")
+            result = statement.execute(button_id, new_developer_id).first
+
+            expect(result["button_id"]).to eq(button_id)
+            expect(result["developer_id"]).to eq (new_developer_id)
+
+        end
+        
+        it "successfully assigns both a new developer and reason" do
+            get "/reassign_button/#{button_id}?reason_id=#{new_reason_id}&developer_id=#{new_developer_id}"
+
+            expect(last_response.status).to eq(200)
+
+            statement = $client.prepare("SELECT button_id, reason_id FROM reason_pairings WHERE button_id=? AND reason_id=?;")
+            result = statement.execute(button_id, new_reason_id).first
+
+            expect(result["button_id"]).to eq(button_id)
+            expect(result["reason_id"]).to eq(new_reason_id)
+
+            statement = $client.prepare("SELECT button_id, developer_id FROM developer_pairings WHERE button_id=? AND developer_id=?;")
+            result = statement.execute(button_id, new_developer_id).first
+
+            expect(result["developer_id"]).to eq (new_developer_id)
+        end
+
+        it "throws errors when button provided is inactive" do
+            button_id = 1 #inactive button
+            get "/reassign_button/#{button_id}?reason_id=#{new_reason_id}&developer_id=#{new_developer_id}"
+            
+            expect(last_response.status).to eq(404)
+        end
+
+        it "throws error when neither reason id or developer id is given" do
+            new_developer_id = nil
+            new_reason_id = nil
+
+            get "/reassign_button/#{button_id}"
+
+            expect(last_response.status).to eq(400)
         end
     end
 end
